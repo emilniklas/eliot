c90ad116125c90b3a725ab367be93bf29fcd946d
Emil Persson <emil.persson@creuna.se>
2016-06-19 04:54:45 +0200

    Fix react preamble

--- src/preambles/react.js
+++ src/preambles/react.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import { env } from '../index'
 
-if (env.ELIOT_TARGET === 'browser') {
+if (process.env.ELIOT_TARGET === 'browser') {
   window.__react = React.createElement
 } else {
   global.__react = React.createElement
