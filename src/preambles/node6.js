ea7bb3c71bf7c738335e19bb305f64c4fa0cbf03
Emil Persson <emil.persson@creuna.se>
2016-06-19 07:09:00 +0200

    Fix bug

--- src/preambles/node6.js
+++ src/preambles/node6.js
@@ -4,8 +4,8 @@ global.__fetch = global.fetch
 
 const devMode = process.env.NODE_ENV === 'development'
 const productionMode = !devMode
-process.env.ELIOT_DEV_MODE = devMode ? 'true' : 'false'
-process.env.ELIOT_PRODUCTION_MODE = productionMode ? 'true' : 'false'
+process.env.ELIOT_DEV_MODE = devMode
+process.env.ELIOT_PRODUCTION_MODE = productionMode
 process.env.ELIOT_TARGET = 'node6'
 process.env.ELIOT_SERVER = true
 process.env.ELIOT_BROWSER = false
